a6a24a5ed9b8e3e70bad826f1115017fdc64d6cb
qaiser_muhammad <qaiser_muhammad@hotmail.com>
2020-08-06 13:10:08 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MilesToFeet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            float miles = 4.5f;
+            const int MILES_RATIO = 5280;
+            const int INCHES_RATIO = 63360;
+
+            Console.WriteLine("{0:F2} miles = {1} feet", miles, miles * MILES_RATIO);
+            Console.WriteLine("{0:F2} miles = {1} inches", miles, miles * INCHES_RATIO);
+
+            Console.ReadLine();
+        }
+    }
+}
